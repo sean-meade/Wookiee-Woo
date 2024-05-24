c9d2b30d954fe54803c14002d730d4a4818482e6
Alma <isakvent@outlook.com>
2024-05-24 14:26:55 +0200

    changes to NavBar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar } from "react-bootstrap";
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from "react-router-dom";
+import styles from "../styles/NavBar.module.css";
 // authentication import goes here
 
 const NavBar = () => {
+    // authentication as useState is temporary:
+    // eslint-disable-next-line
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+    const authenticatedOptions = (<>
+        <NavLink
+        to="/profiles"
+        className={styles.NavBarButton}
+        >
+            My profile
+        </NavLink>
+        <NavLink
+        to="/sign-out"
+        className={styles.NavBarButton}
+        >
+            Sign out
+        </NavLink>
+    </>)
+
+    const unauthenticatedOptions = (<>
+        <NavLink
+        to="/sign-in"
+        className={styles.NavBarButton}
+        >
+            Sign in
+        </NavLink>
+        <NavLink
+        to="/sign-up"
+        className={styles.NavBarButton}
+        >
+            Sign up
+        </NavLink>
+    </>)
 
     return(<>
         <Nav>
-            <NavLink to="/profiles">/profiles</NavLink>
+            <Navbar.Brand className={styles.NavBarLogo}>Logo</Navbar.Brand>
+            <NavLink
+            to="/profiles"
+            className={styles.NavBarButton}
+            >
+                profiles
+            </NavLink>
+            <NavLink
+            to="/"
+            className={styles.NavBarButton}
+            >
+                home
+            </NavLink>
+            {isAuthenticated ? (authenticatedOptions) : (unauthenticatedOptions)}
+            
+            
         </Nav>
     </>);
 };
