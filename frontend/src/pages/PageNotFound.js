25a0cd138baffa162618a6a86582919773389439
Rdawson96 <rdawson164@gmail.com>
2024-05-24 18:57:22 +0100

    linked stylesheet to pagenotfound and changed wording of button

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import styles from "../styles/PageNotFound.module.css";
 
 const PageNotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <p>This is not the page you are looking for.</p>
-            <Link to="/">
-                Go back to Home
+        <div className={styles.container}>
+            <h1 className={styles.heading}>404</h1>
+            <p className={styles.paragraph}>This is not the page you are looking for.</p>
+            <Link to="/" className={`btn btn-primary ${styles.link}`}>
+                Go back to the Home Page
             </Link>
         </div>
     );
