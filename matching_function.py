9bde49a6bd6db0793cbe45f6263a67894227e0e8
Sean Tilson <seantilson@gmail.com>
2024-05-24 20:36:03 +0000

    add comment to normalize weight function

--- matching_function.py
+++ matching_function.py
@@ -18,6 +18,7 @@ def normalize_weights(weights: dict, factor: float=1.0) -> dict:
     weight is 1 and all others are relative to it. factor is included as a 
     parameter as you may want the max value to be more heavily weighted.
     """
+    # does this function do what Sean thinks it does
     max_weight = max([value for value in weights.values()])
     scaling_factor = max_weight*factor
     return {key:value/scaling_factor for key, value in weights.items()}
