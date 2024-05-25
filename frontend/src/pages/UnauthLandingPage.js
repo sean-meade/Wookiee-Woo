5276cff1e60051274d68767fe07e1410eec6108a
Rdawson96 <rdawson164@gmail.com>
2024-05-25 22:15:16 +0100

    Added unauthorsied landing page and linked to stylesheet

---
+++ frontend/src/pages/UnauthLandingPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'; 
+import './LandingPage.module.css';
+
+const UnauthorisedLandingPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="landing-page-container">
+        <div className="landing-page-content">
+          <h1>Welcome Wookie Woo</h1>
+          <p>Where you find your perfect match in a galaxy of singles!</p>
+          <div className="landing-page-video"> // video placeholder
+          </div>
+          <p>Sign in or sign up to get started!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UnauthorisedLandingPage;
