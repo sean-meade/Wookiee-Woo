d91d3bf9019680c4745fbff41ae9a2dd2aca6079
Alma <isakvent@outlook.com>
2024-05-25 14:33:17 +0200

    rearranged routing in App.js to be compatible with the navigation/ main view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,29 +10,21 @@ import PageNotFound from './pages/PageNotFound';
 function App() {
   return (
     <Router>
+      <div className="App-container">
+        <div className="Nav-bar-container">
+          <NavBar />
+        </div>
+        <div className="Main-view-container">
       <Routes>
+        <Route path="/" element={<p>Landing page</p>} />
+        <Route path="/sign-in" element={<p>Sign in page</p>} />
+        <Route path="/sign-up" element={<p>Sign up page</p>} />
+        <Route path="/sign-out" element={<p>Sign out page</p>} />
         <Route path="/profiles" element={<Profiles />} />
-        <Route path="/" element={
-          <div>
-            <div className="App-container">
-              <div className="Nav-bar-container">
-                <NavBar />
-              </div>
-              <div className="Main-view-container">
-                <p>This is a placeholder text for Main view container. This container will render:</p>
-                <ul>
-                  <li>landing page unauthenticated</li>
-                  <li>landing page authenticated</li>
-                  <li>authentication sign in, out, up</li>
-                  <li>edit profile</li>
-                  <li>...and more</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        } />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
