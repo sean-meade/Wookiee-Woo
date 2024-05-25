8370039b6c3b1d1933b58e334379338b8f3477b2
Sean Tilson <seantilson@gmail.com>
2024-05-25 11:34:22 +0000

    add SurveyResults model

--- backend/matchmaking/models.py
+++ backend/matchmaking/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from ..profiles.models import CustomUser
 
 # Create your models here.
+class SurveyResults(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    a_new_hope = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    empire_strikes_back = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    return_of_the_jedi = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    phantom_menace = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    attack_of_the_clones = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    revenge_of_the_sith = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    force_awakens = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    last_jedi = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    rise_of_skywalker = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    rogue_one = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    solo = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    mandalorian = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    book_of_boba_fett = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    obi_wan_kenobi = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    andor = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    ahsoka = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    clone_wars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    rebels = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    resistance = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    bad_batch = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    visions = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    tales_of_the_empire = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
+    tales_of_the_jedi = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],null=True)
