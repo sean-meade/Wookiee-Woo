be1c045d656f4601662a270e815fd2c8158fabeb
Rdawson96 <rdawson164@gmail.com>
2024-05-25 22:31:45 +0100

    updated classes links in unauthlandingpage.js

--- frontend/src/pages/UnauthLandingPage.js
+++ frontend/src/pages/UnauthLandingPage.js
@@ -7,11 +7,11 @@ const UnauthorisedLandingPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="landing-page-container">
-        <div className="landing-page-content">
+      <div className={styles.landing-page-container}>
+        <div className={styles.landing-page-content}>
           <h1>Welcome Wookie Woo</h1>
           <p>Where you find your perfect match in a galaxy of singles!</p>
-          <div className="landing-page-video"> // video placeholder
+          <div className={styles.landing-page-video}> // video placeholder
           </div>
           <p>Sign in or sign up to get started!</p>
         </div>
