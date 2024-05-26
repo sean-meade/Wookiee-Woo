5bb677d3e9444909185071e8a451dbd7840160fb
Alma <isakvent@outlook.com>
2024-05-26 18:04:19 +0200

    hide the detail or list behind a temporary toggle button

--- frontend/src/pages/match/matchComponents/MatchComponent.js
+++ frontend/src/pages/match/matchComponents/MatchComponent.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../global.css";
 import styles from "../../../styles/MatchComponent.module.css";
+import MatchDetailComponent from "./MatchDetailComponent";
+import MatchListComponent from "./MatchListComponent";
 
 const MatchComponent = () => {
+    // placholder state
+    // Match List if false
+    // Match Detail if true
+    const [toggleMatch, setToggleMatch] = useState(false);
+    const handleToggle = () => {
+        setToggleMatch(prevState => !prevState);
+      };
 
     return(<>
     <div className={styles.MatchComponentContainer}>
-        <p>Skeleton for MatchComponent.js</p>
+        {toggleMatch ? (<>
+        <MatchDetailComponent />
+        </>) : (<>
+        <MatchListComponent />
+        </>)}
     </div>
+    <button onClick={handleToggle}>toggle me</button>
     </>);
 };
 
