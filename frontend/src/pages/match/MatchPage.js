aae2759220ab83ad31f0d7d7647bff6bd09c9d94
Alma <isakvent@outlook.com>
2024-05-26 17:45:29 +0200

    align user and match to block or flex display depending on screen size

--- frontend/src/pages/match/MatchPage.js
+++ frontend/src/pages/match/MatchPage.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import MatchComponent from "./matchComponents/MatchComponent.js";
-import MatchDetailComponent from "./matchComponents/MatchDetailComponent.js";
-import MatchListComponent from "./matchComponents/MatchListComponent.js";
 import UserComponent from "./matchComponents/UserComponent.js";
 import "../../global.css";
 import styles from "../../styles/MatchPage.module.css"
@@ -10,21 +8,12 @@ const MatchPage = () => {
 
     return(<>
     <div className={styles.MatchPageContainer}>
-        <p>Skeleton for MatchPage.js</p>
-        <ul>
-            <li>
-                <MatchComponent />
-            </li>
-            <li>
-                <MatchDetailComponent />
-            </li>
-            <li>
-                <MatchListComponent />
-            </li>
-            <li>
-                <UserComponent />
-            </li>
-        </ul>
+        <div className={styles.UserContainer}>
+            <UserComponent />
+        </div>
+        <div className={styles.MatchContainer}>
+            <MatchComponent />
+        </div>
     </div>
     </>);
 };
