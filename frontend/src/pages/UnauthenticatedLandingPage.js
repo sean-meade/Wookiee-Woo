c5b3c1d51228b79182d621797f83b6e024b0c27e
Rob Dawson <rdawson164@gmail.com>
2024-05-26 08:14:53 +0000

    Renamed page and styling to make clearer for when creating authorised version

--- frontend/src/pages/UnauthenticatedLandingPage.js
+++ frontend/src/pages/UnauthenticatedLandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar'; 
-import './LandingPage.module.css';
+import './UnauthenticatedLandingPage.module.css';
 
 const UnauthorisedLandingPage = () => {
   return (
