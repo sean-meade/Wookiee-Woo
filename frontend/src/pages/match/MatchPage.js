0f8d9103976546bb93b63a1f6e528240b27a2f05
Alma <isakvent@outlook.com>
2024-05-26 17:47:36 +0200

    remove accidental import of useState

--- frontend/src/pages/match/MatchPage.js
+++ frontend/src/pages/match/MatchPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import MatchComponent from "./matchComponents/MatchComponent.js";
 import UserComponent from "./matchComponents/UserComponent.js";
 import "../../global.css";
