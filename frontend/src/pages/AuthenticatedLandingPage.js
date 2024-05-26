52163abe314be6e370fe893986270c13436280d3
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-05-27 00:45:14 +0200

    AuthenticatedLandingPage/UnAuthenticated styled.

--- frontend/src/pages/AuthenticatedLandingPage.js
+++ frontend/src/pages/AuthenticatedLandingPage.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/AuthenticatedLandingPage.module.css';
 
 const AuthenticatedLandingPage = () => {
--- frontend/src/pages/UnauthenticatedLandingPage.js
+++ frontend/src/pages/UnauthenticatedLandingPage.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../UnauthenticatedLandingPage.module.css';
+import '../styles/UnauthenticatedLandingPage.css';
 
 const UnauthorisedLandingPage = () => {
   return (
-    <div>
-      <div className={styles.landing-page-container}>
-        <div className={styles.landing-page-content}>
-          <h1>Welcome Wookie Woo</h1>
-          <p>Where you find your perfect match in a galaxy of singles!</p>
-          <div className={styles.landing-page-video}> // video placeholder
-          </div>
-          <p>Sign in or sign up to get started!</p>
-        </div>
+    <div className="landingPageContainer">
+
+      <div className="main-content">
+
+        <h1>Welcome to Wookie Woo</h1>
+        <p>The Coding Wookie Woo Resistance</p>
+        <div className="landing-page-video"> {/* video placeholder */} </div>
+        <p>Sign in or sign up to get started!</p>
       </div>
     </div>
   );
