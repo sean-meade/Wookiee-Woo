82edf9dd95914ce76a42127a888d3e2284d96474
Alma <isakvent@outlook.com>
2024-05-27 12:10:24 +0200

    tweak styling for 404 page

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "../styles/PageNotFound.module.css";
 import { Link } from 'react-router-dom';
+import '../global.css';
 
 const PageNotFound = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.heading}>404</h1>
             <p className={styles.paragraph}>This is not the page you are looking for.</p>
-            <Link to="/" className={`btn btn-primary ${styles.link}`}>
+            <br/>
+            <Link to="/" className={styles.Link}>
                 Go back to the Home Page
             </Link>
         </div>
